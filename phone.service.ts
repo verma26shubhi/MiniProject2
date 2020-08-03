399f7b81ca0f7392501b635bafe3a745289e3a22
verma26shubhi <43296683+verma26shubhi@users.noreply.github.com>
2020-08-03 21:24:59 +0530

    Update phone.service.ts

--- phone.service.ts
+++ phone.service.ts
@@ -5,12 +5,12 @@ export class ContactService{
         this.csarray=[];
         var c1,c2;
         c1=new Contact();
-        c1.name="Raju";
-        c1.phone=987652431;
+        c1.name="Shubhi";
+        c1.phone=9876533331;
         this.csarray.push(c1); 
         c2=new Contact();
-        c2.name="Raj";
-        c2.phone=982635412;
+        c2.name="Shashank";
+        c2.phone=982631112;
         this.csarray.push(c2);     
     }
     showcontact(){
